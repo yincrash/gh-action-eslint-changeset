401355de09a9ab60a9870aeacebcddd985617af3
Esteban Rincon <esteban.rincon.mora@gmail.com>
2022-04-27 13:48:50 -0500

    test 7

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ git config --global --add safe.directory /github/workspace
 
 mainbranch=$1
 
-git checkout $mainbranch
 
-branch=$(git branch | grep '*' | sed s/*'\s'//g)
+branch=$(git branch | grep '*' | sed s/*'\s'//g) 
+git checkout $mainbranch
 
 files=$(git diff --name-only $branch $(git merge-base $branch $mainbranch) | grep '\.'$2'$')
 
