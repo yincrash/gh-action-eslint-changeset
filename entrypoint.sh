8271229723d2a21f5ca2c2a9401c9601004e670e
Esteban Rincon <esteban.rincon.mora@gmail.com>
2022-04-27 14:24:14 -0500

    test 14

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ branch=$(git branch | grep '*' | sed s/*'\s'//g)
 
 git checkout $branch 
 
+npm i --only=dev
+
 files=$(git diff --name-only $branch $(git merge-base $branch $mainbranch) | grep '\.'$2'$')
 
 npx eslint $files
