b5978b501115454293a7ba77a7b7365dac269685
Esteban Mora <esteban.mora@cox.com>
2022-05-09 11:38:12 -0500

    Add npm access token support

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ branch=$(git branch | grep '*' | sed s/*'\s'//g)
 
 git checkout $branch 
 
+if [ ! -z "$3" ]; then
+    echo "//registry.npmjs.org/:_authToken=$3" > ./.npmrc
+fi
+
 npm i -D
 
 files=$(git diff --name-only $branch $(git merge-base $branch $mainbranch) | grep -E '.+\.('$2')$')
